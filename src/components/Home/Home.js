1ca44f9c211399b0bc2ed04f9c8920ccf4ce6154
rasel <russelroy15@gmail.com>
2021-10-17 01:44:29 +0600

    WIP on doctor shop project

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import About from "../About/About";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import Contact from "../Contact/Contact";
 import Services from "../Services/Services";
 import Menubar from "../Shared/Menubar/Menubar";
 import "./Home.css";
@@ -13,6 +14,7 @@ const Home = () => {
       <Services />
       <About />
       <Category />
+      <Contact />
     </div>
   );
 };
