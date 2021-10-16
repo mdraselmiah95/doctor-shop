0ff1fd918ae750b9962325cdd7d24defa073e00e
rasel <russelroy15@gmail.com>
2021-10-16 23:00:29 +0600

    WIP on Doctor Shop Project

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "../Banner/Banner";
 import Menubar from "../Shared/Menubar/Menubar";
 import "./Home.css";
 const Home = () => {
   return (
     <div>
       <Menubar />
+      <Banner />
     </div>
   );
 };
