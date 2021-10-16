d9928e7590e9d8e4c711a6626ccf53d89e2e5a96
rasel <russelroy15@gmail.com>
2021-10-16 21:58:12 +0600

    WIP on Doctor Shop Project

--- src/components/Shared/Menubar/Menubar.js
+++ src/components/Shared/Menubar/Menubar.js
@@ -1,9 +1,21 @@
 import React from "react";
+import logo from "../../../images/Banner&Background/logo.png";
 import "./Menubar.css";
 const Menubar = () => {
   return (
     <div>
-      <h2>This is Menubar</h2>
+      <div className="menubar-container">
+        <div className="menubar">
+          <div className="row">
+            <div className="col-md-2">
+              <div className="logo-img">
+                <img src={logo} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        r
+      </div>
     </div>
   );
 };
