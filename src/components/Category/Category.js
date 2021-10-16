e04fbbc28d3733aa9e5ab53601942c35a8351127
rasel <russelroy15@gmail.com>
2021-10-17 01:24:56 +0600

    WIP on doctor shop project

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -3,7 +3,35 @@ import "./Category.css";
 const Category = () => {
   return (
     <div>
-      <h2>This is Category</h2>
+      <div className="category-container">
+        <div className="top-part-category">
+          <h5 className="mt-4">
+            <span className="me-3">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0447/3569/2962/t/2/assets/waves.png"
+                alt=""
+              />
+            </span>
+            OUR PRODUCTS{" "}
+            <span className="ms-3">
+              <img
+                src="https://cdn.shopify.com/s/files/1/0447/3569/2962/t/2/assets/waves.png"
+                alt=""
+              />
+            </span>
+          </h5>
+          <h1 className="mt-5">Shop By Category</h1>
+          <p className="mt-3">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa
+          </p>
+          <div className="button-continer">
+            <button className="btn-0">New Arrivals</button>
+            <button className="btn-1">Featured</button>
+            <button className="btn-1">Best Sellers</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
