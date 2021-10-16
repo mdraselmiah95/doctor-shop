2b183a91400c7db9d5ad687fac476e77a29fde27
rasel <russelroy15@gmail.com>
2021-10-16 23:19:59 +0600

    WIP on Doctor Shop Project

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,18 @@ import "./Banner.css";
 const Banner = () => {
   return (
     <div>
-      <h2>This is banner</h2>
+      <div className="banner d-flex  align-items-center ">
+        <div className="row container">
+          <div className="col-md-10">
+            <h1 className="title">Take care of your Health Now !</h1>
+            <h2 className="title-sm"> Sale up to 30% all products</h2>
+            <button className="shop-btn btn btn-outline-warning">
+              Shop Now
+            </button>
+          </div>
+          <div className="col-md-2"></div>
+        </div>
+      </div>
     </div>
   );
 };
