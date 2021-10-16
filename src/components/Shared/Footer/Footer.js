4f2f561ee6dbec4dcd6100f47bb5b2d76dfc1851
rasel <russelroy15@gmail.com>
2021-10-17 02:23:14 +0600

    WIP on doctor shop project

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,4 +1,16 @@
-import { faFacebookF, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faAddressCard,
+  faEnvelope,
+  faFax,
+  faLink,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Footer.css";
@@ -32,20 +44,20 @@ const Footer = () => {
                 placeholder="Hello world"
               />
               <br />
-              <input className="input-button" type="text" />
+              <input className="input-button" type="submit" />
               <br />
-              <p>
-                <span className="m-2 ">
+              <p className="mt-4">
+                <span className="m-2 fs-4 text-primary bg-light px-3 py-2 rounded-3">
                   <FontAwesomeIcon icon={faFacebookF} />
                 </span>
-                <span className="m-2 ">
+                <span className="m-2 fs-4 text-danger bg-light px-3 py-2 rounded-3">
                   <FontAwesomeIcon icon={faYoutube} />
                 </span>
-                <span className="m-2 ">
-                  <FontAwesomeIcon icon={faYoutube} />
+                <span className="m-2 fs-4 text-info bg-light px-3 py-2 rounded-3">
+                  <FontAwesomeIcon icon={faLinkedinIn} />
                 </span>
-                <span className="m-2 ">
-                  <FontAwesomeIcon icon={faYoutube} />
+                <span className="m-2 fs-4 text-warning bg-light px-3 py-2 rounded-3">
+                  <FontAwesomeIcon icon={faInstagram} />
                 </span>
               </p>
             </div>
@@ -53,23 +65,64 @@ const Footer = () => {
           <div className="col-md-3">
             <div className="third-part">
               <h1>Useful Links</h1>
-              <p className="mt-4">Privacy Policy</p>
-              <p>Privacy Policy</p>
-              <p>Privacy Policy</p>
-              <p>Privacy Policy</p>
-              <p>Privacy Policy</p>
+
+              <p className="mt-4">
+                <FontAwesomeIcon icon={faLink} className="text-success me-2" />
+                Privacy Policy
+              </p>
+
+              <p>
+                <FontAwesomeIcon icon={faLink} className="text-success me-2" />
+                Privacy Policy
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faLink} className="text-success me-2" />
+                Privacy Policy
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faLink} className="text-success me-2" />
+                Privacy Policy
+              </p>
+              <p>
+                <FontAwesomeIcon icon={faLink} className="text-success me-2" />
+                Privacy Policy
+              </p>
             </div>
           </div>
           <div className="col-md-3">
             <div className="fourth-part">
               <h1>Contact Us</h1>
-              <small className="mt-5">Office: 12 Fake Street,New York</small>
+              <small className="mt-5">
+                <FontAwesomeIcon
+                  icon={faAddressCard}
+                  className="text-info me-2 fs-5"
+                />
+                Office: 12 Fake Street,New York
+              </small>
               <br />
-              <small className="mt-3">Phone: (08) 8827 633354</small>
+              <small className="mt-3">
+                <FontAwesomeIcon
+                  icon={faPhone}
+                  className="text-info me-2 fs-5"
+                />
+                Phone: (08) 8827 633354
+              </small>
               <br />
-              <small className="mt-3">Fax: 08) 08 4752 1499</small>
+              <small className="mt-3">
+                <FontAwesomeIcon
+                  icon={faFax}
+                  className="text-danger me-2 fs-5"
+                />
+                Fax: 08) 08 4752 1499
+              </small>
               <br />
-              <small className="mt-3">Mail: support.goaltheme.com</small>
+              <small className="mt-3">
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className="text-primary me-2 fs-5"
+                />
+                Mail: support.goaltheme.com
+              </small>
             </div>
           </div>
         </div>
