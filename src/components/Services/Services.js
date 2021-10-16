a9002b9a6c5e81a97645fa903f435ab6b461a95b
rasel <russelroy15@gmail.com>
2021-10-17 00:07:42 +0600

    WIP on Doctor Shop Project

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -32,11 +32,11 @@ const Services = () => {
             ligula eget dolor. Aenean massa.
           </p>
         </div>
-        <div className="service-cart">
+        <div className="service-cart mb-5">
           <div className="row">
             {data.map((pd) => (
               <div key={pd.id} className="col-lg-4 col-md-6">
-                <div className="cart p-3 border m-2">
+                <div className="cart p-3 border border m-2">
                   <div className="cart-img">
                     <img src={pd.image} alt="" className="my-2" />
                     <h5>{pd.name}</h5>
