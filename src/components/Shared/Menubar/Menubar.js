fbfbe9ef5a55fe38637b03850131278dfb9e1d48
rasel <russelroy15@gmail.com>
2021-10-16 22:57:03 +0600

    WIP on Doctor Shop Project

--- src/components/Shared/Menubar/Menubar.js
+++ src/components/Shared/Menubar/Menubar.js
@@ -5,20 +5,20 @@ const Menubar = () => {
   return (
     <div>
       <div className="menubar-container">
-        <div className="menubar">
+        <div className="menubar container">
           <div className="row">
             <div className="col-md-2">
               <div className="logo-img">
-                <img src={logo} alt="logo" />
+                <img className="p-2" src={logo} alt="logo" />
               </div>
             </div>
             <div className="col-md-10">
               <div className="menu-items">
                 <ul className="d-flex align-items-end justify-content-end">
-                  <li className="items">Home</li>
-                  <li className="items">Shop</li>
-                  <li className="items">Contact</li>
-                  <li className="items me-4">About</li>
+                  <li className="items p-2">Home</li>
+                  <li className="items p-2">Shop</li>
+                  <li className="items p-2">Contact</li>
+                  <li className="items p-2">About</li>
                 </ul>
               </div>
             </div>
